60e35160bdc05a606bb55f46eb59c37dec69eb28
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-14 11:14:18 +0200

    Create main.java

---
+++ 13_while_loop/src/main.java
@@ -0,0 +1,13 @@
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)  {
+	int counter = 0;
+    
+    while(counter<10){
+      System.out.println(counter);
+      counter++;      	
+   }
+}
+}
