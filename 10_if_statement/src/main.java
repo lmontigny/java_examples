1e8ddd99cddef428842f67ab9b4da6bb3661bc6d
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-14 11:04:28 +0200

    Create main.java

---
+++ 10_if_statement/src/main.java
@@ -0,0 +1,13 @@
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)  {
+	int test = 6;
+    if(test == 9){
+      System.out.println("yes");
+  	}
+    else{
+      System.out.println("no");
+    }
+}
