899cc68f1c58baeda9c88f4da678398eeef97741
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-14 11:07:19 +0200

    Create main.java

---
+++ 11_logical_operators/src/main.java
@@ -0,0 +1,14 @@
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)  {
+	int test = 6;
+    if(test > 3 && test < 10 || test == 9){
+      System.out.println("yes");
+  	}
+    else{
+      System.out.println("no");
+    }
+	}
+}
