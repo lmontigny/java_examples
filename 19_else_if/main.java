98c98eb974e11e10b9dcfada21d32aa9e09305c9
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-14 11:45:17 +0200

    Create main.java

---
+++ 19_else_if/main.java
@@ -0,0 +1,21 @@
+import java.lang.Math; // headers MUST be above the first class
+import java.util.Scanner;
+
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {  
+	int age = 60;
+    if(age<60){
+     	System.out.println("young"); 
+    }
+    else if(age == 60){
+      System.out.println("60"); 
+    }
+    
+    
+    
+  }
+}
