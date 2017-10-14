550c0691cf0fb1171782a066224768dfae703bc9
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-14 11:11:54 +0200

    Create main.java

---
+++ 12_switch/src/main.java
@@ -0,0 +1,23 @@
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)  {
+	int age;
+    age = 1;
+    
+    switch(age){
+      case 1:
+      	System.out.println("crawl");
+      	break;
+      case 2:
+        System.out.println("eat");
+      	break;
+      default:
+        System.out.println("default");
+      	break;
+      
+    }
+      	
+   }
+}
